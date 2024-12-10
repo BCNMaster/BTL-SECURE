0a40e5152b101f36007d928d1f3330b7f0637316
BCNMaster <bottlechainnetwork@gmail.com>
2024-12-10 00:26:49 -0500

    Add root layout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { WalletProvider } from '../contexts/WalletProvider';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <WalletProvider>{children}</WalletProvider>
+      </body>
+    </html>
+  );
+}

b5e0aaa0740b92f11e9c6b039cbee66e226ccab1
BCNMaster <bottlechainnetwork@gmail.com>
2025-01-19 17:09:56 -0500

    Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { Dashboard } from '@/components/Dashboard';
+import { Landing } from '@/components/Landing';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  useEffect(() => {
+    // Check if wallet is connected from local storage
+    const walletStatus = localStorage.getItem('walletConnected');
+    if (walletStatus === 'true') {
+      setIsWalletConnected(true);
+    }
+  }, []);
+
+  return isWalletConnected ? <Dashboard /> : <Landing />;
 }
